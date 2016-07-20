9f16f4cc543520a153dafb401623d7e2b7e5d573
youhouhou <zehongma@gmail.com>
2016-07-20 09:01:19 -0700

    added css

--- js/changepic.js
+++ js/changepic.js
@@ -1,6 +1,9 @@
 
 //whichpic represent a element node
 function showPic(whichpic){
+	var text = whichpic.getAttribute("title");
+	var description = document.getElementById("description");
+	description.firstChild.nodeValue = text;
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src",source);
